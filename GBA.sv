e20ca9cf8a363989c3f103d40984d6076d5e3ff4
sorgelig <pour.garbage@gmail.com>
2019-12-18 03:04:39 +0800

    Add GB/GBC extensions to browser.

--- GBA.sv
+++ GBA.sv
@@ -168,7 +168,7 @@ wire reset = RESET | buttons[1] | status[0] | cart_download | bk_loading | hold_
 `include "build_id.v"
 parameter CONF_STR = {
     "GBA;;",
-    "FS,GBA;",
+    "FS,GBAGB GBC;",
     "-;",
     //"C,Cheats;",
     //"H1O6,Cheats Enabled,Yes,No;",
